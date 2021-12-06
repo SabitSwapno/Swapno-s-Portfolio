9c6cfb51fe588bbc0589e9f790d263d511027e11
SabitSwapno <sabitswapno3@gmail.com>
2021-12-06 22:05:14 +0600

    one particle removed

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,100 +1,11 @@
 import React from 'react';
 import Banner from './Banner/Banner';
 import NavigationBar from './NavigationBar/NavigationBar';
-import Particles from "react-tsparticles";
 
 const Header = () => {
-
-    const particlesInit = (main) => {
-        console.log(main);
-
-        // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
-    };
-
-    const particlesLoaded = (container) => {
-        console.log(container);
-    };
-
     return (
         <>
             <NavigationBar></NavigationBar>
-            <Particles
-                id="tsparticles"
-                init={particlesInit}
-                loaded={particlesLoaded}
-                options={{
-                    fpsLimit: 60,
-                    interactivity: {
-                        events: {
-                            onClick: {
-                                enable: true,
-                                mode: "push",
-                            },
-                            onHover: {
-                                enable: true,
-                                mode: "repulse",
-                            },
-                            resize: true,
-                        },
-                        modes: {
-                            bubble: {
-                                distance: 400,
-                                duration: 2,
-                                opacity: 0.8,
-                                size: 40,
-                            },
-                            push: {
-                                quantity: 4,
-                            },
-                            repulse: {
-                                distance: 70,
-                                duration: 0.4,
-                            },
-                        },
-                    },
-                    particles: {
-                        color: {
-                            value: "#72b626",
-                        },
-                        links: {
-                            color: "#72b626",
-                            distance: 150,
-                            enable: true,
-                            opacity: 0.5,
-                            width: 1,
-                        },
-                        collisions: {
-                            enable: true,
-                        },
-                        move: {
-                            direction: "none",
-                            enable: true,
-                            outMode: "bounce",
-                            random: false,
-                            speed: 1,
-                            straight: false,
-                        },
-                        number: {
-                            density: {
-                                enable: true,
-                                value_area: 800,
-                            },
-                            value: 80,
-                        },
-                        opacity: {
-                            value: 0.5,
-                        },
-                        shape: {
-                            type: "circle",
-                        },
-                        size: {
-                            random: true,
-                            value: 6,
-                        },
-                    },
-                    detectRetina: true,
-                }}
-            />
             <Banner>
             </Banner>
         </>
